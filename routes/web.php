47897183aa2a797c3bb72ef49d42e39df3d69ae1
cleyxcode <afiafikadewi3@gmail.com>
2026-02-20 01:43:21 +0900

    update navbar admin di atas

--- routes/web.php
+++ routes/web.php
@@ -14,18 +14,18 @@ use App\Http\Controllers\Frontend\GaleriController;
 use App\Http\Controllers\Frontend\PasswordResetController;
 
 // Storage route
-// Route::get('/storage/{folder}/{filename}', function ($folder, $filename) {
-//     $path = storage_path('app/public/' . $folder . '/' . $filename);
+Route::get('/storage/{folder}/{filename}', function ($folder, $filename) {
+    $path = storage_path('app/public/' . $folder . '/' . $filename);
     
-//     if (!file_exists($path)) {
-//         abort(404);
-//     }
+    if (!file_exists($path)) {
+        abort(404);
+    }
     
-//     $file = file_get_contents($path);
-//     $type = mime_content_type($path);
+    $file = file_get_contents($path);
+    $type = mime_content_type($path);
     
-//     return response($file, 200)->header('Content-Type', $type);
-// })->where('folder', '.*')->where('filename', '.*');
+    return response($file, 200)->header('Content-Type', $type);
+})->where('folder', '.*')->where('filename', '.*');
 
 // ─── Root redirect ────────────────────────────────────────────────────────────
 Route::get('/', function () {
